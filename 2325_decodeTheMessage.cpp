70754756fc64bd25e9e8e39ba324f9bb710fc7ea
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-04-26 12:40:45 +0530

    Solved 2325. Decode the message and 2367. Number of arithmatic triplets

---
+++ 2325_decodeTheMessage.cpp
@@ -0,0 +1,60 @@
+/*
+You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+
+Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+Align the substitution table with the regular English alphabet.
+Each letter in message is then substituted using the table.
+Spaces ' ' are transformed to themselves.
+For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+Return the decoded message.
+
+    Example 1:
+
+Input: key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
+Output: "this is a secret"
+Explanation: The diagram above shows the substitution table.
+It is obtained by taking the first appearance of each letter in "the quick brown fox jumps over the lazy dog".
+
+    Example 2:
+
+Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
+Output: "the five boxing wizards jump quickly"
+Explanation: The diagram above shows the substitution table.
+It is obtained by taking the first appearance of each letter in "eljuxhpwnyrdgtqkviszcfmabo".
+ 
+    Constraints:
+
+26 <= key.length <= 2000
+key consists of lowercase English letters and ' '.
+key contains every letter in the English alphabet ('a' to 'z') at least once.
+1 <= message.length <= 2000
+message consists of lowercase English letters and ' '.
+*/
+
+#include<map>
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        vector<char> alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        vector<char> leg;
+        for(int i = 0; i<key.length(); i++){
+            if((i==0 || find(leg.begin(), leg.end(), key[i])==leg.end()) && key[i]!=' '){
+                leg.push_back(key[i]);
+            }
+        }
+        string ans = "";
+        for(int i = 0; i<message.size(); i++){
+            bool flag = true;
+            if(message[i] == ' '){
+                ans = ans + " ";
+            }
+            for(int j = 0; j<26 && flag; j++){
+                if(message[i] == leg[j]){
+                    ans = ans + alpha[j];
+                    flag = false;
+                }
+            }
+        }
+        return ans;
+    }
+};

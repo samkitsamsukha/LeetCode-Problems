1786cea6eadec79d2dc5100a7a547050cbb83a6d
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-09-13 19:45:03 +0530

    Solved 1310

---
+++ 1310_XORQueriesOfASubarray.cpp
@@ -0,0 +1,66 @@
+/*
+You are given an array arr of positive integers. You are also given the array queries where queries[i] = [lefti, righti].
+
+For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+
+Return an array answer where answer[i] is the answer to the ith query.
+
+ 
+
+Example 1:
+
+Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+Output: [2,7,14,8] 
+Explanation: 
+The binary representation of the elements in the array are:
+1 = 0001 
+3 = 0011 
+4 = 0100 
+8 = 1000 
+The XOR values for queries are:
+[0,1] = 1 xor 3 = 2 
+[1,2] = 3 xor 4 = 7 
+[0,3] = 1 xor 3 xor 4 xor 8 = 14 
+[3,3] = 8
+
+Example 2:
+
+Input: arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+Output: [8,0,4,4]
+
+ 
+
+Constraints:
+
+    1 <= arr.length, queries.length <= 3 * 104
+    1 <= arr[i] <= 109
+    queries[i].length == 2
+    0 <= lefti <= righti < arr.length
+
+*/
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& q) {
+        vector<int> pre;
+        int x = arr[0];
+        pre.push_back(x);
+        for(int i = 1; i<arr.size(); i++){
+            x = x^arr[i];
+            pre.push_back(x);
+        }
+        for(int i = 0; i<pre.size(); i++){
+            cout << pre[i] << endl;
+        }
+        vector<int> ans;
+        for(int i = 0; i<q.size(); i++){
+            if(q[i][0] == 0){
+                ans.push_back(pre[q[i][1]]);
+            }
+            else{
+                ans.push_back(pre[q[i][1]]^pre[q[i][0]-1]);
+            }
+        }
+        return ans;
+    }
+};

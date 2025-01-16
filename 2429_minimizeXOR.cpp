cb6af05f00c313c58d3ef028b2c46c08369ff177
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-01-16 15:44:53 +0530

    Solved 2425

--- 2429_minimizeXOR.cpp
+++ 2429_minimizeXOR.cpp
@@ -37,3 +37,50 @@ Constraints:
 
 */
 
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int sb = 0;
+        while(num2>0){
+            if(num2%2 == 1){
+                sb++;
+            }
+            num2 = num2/2;
+        }
+        cout << sb << endl;
+        string s = "";
+        while(num1>0){
+            s+=(num1%2)+'0';
+            num1 = num1/2;
+        }
+        reverse(s.begin(), s.end());
+        if (s.length() < 32) {
+            s.insert(0, 32 - s.length(), '0');
+        }
+        for(int i = 0; i<s.length(); i++){
+            if(s[i] == '1'){
+                if(sb>0){
+                    sb--;
+                }
+                else{
+                    s[i] = '0';
+                }
+            }
+        }
+        if(sb>0){
+            for(int i = s.length()-1; i>=0 && sb>0; i--){
+                if(s[i] == '0'){
+                    s[i] = '1';
+                    sb--;
+                }
+            }
+        }
+        long long int m = 1;
+        long long int ans = 0;
+        for(int i = s.length()-1; i>=0; i--){
+            ans = ans + m*(s[i]-'0');
+            m*=2;
+        }
+        return ans;
+    }
+};

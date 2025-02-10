a025a07a521ec4304263a6ef8ae905d22ff7572c
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-02-10 14:37:42 +0530

    Solved 3174 with stack

--- 3001-3500/3174_clearDigits.cpp
+++ 3001-3500/3174_clearDigits.cpp
@@ -53,3 +53,25 @@ public:
         return ans;
     }
 };
+
+class Solution {
+    public:
+        string clearDigits(string s) {
+            stack<char> st;
+            for(int i = 0; i<s.length(); i++){
+                if(isdigit(s[i]) && !st.empty()){
+                    st.pop();
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
+            s = "";
+            while(!st.empty()){
+                s = s+st.top();
+                st.pop();
+            }
+            reverse(s.begin(), s.end());
+            return s;
+        }
+    };

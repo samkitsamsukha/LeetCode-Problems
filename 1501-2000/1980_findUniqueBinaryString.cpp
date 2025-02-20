bb4d92cc3676faaf7cab58bea67adcdd81e20a72
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-02-20 10:24:37 +0530

    Solved 1980

---
+++ 1501-2000/1980_findUniqueBinaryString.cpp
@@ -0,0 +1,65 @@
+/*
+Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+
+ 
+
+Example 1:
+
+Input: nums = ["01","10"]
+Output: "11"
+Explanation: "11" does not appear in nums. "00" would also be correct.
+
+Example 2:
+
+Input: nums = ["00","01"]
+Output: "11"
+Explanation: "11" does not appear in nums. "10" would also be correct.
+
+Example 3:
+
+Input: nums = ["111","011","001"]
+Output: "101"
+Explanation: "101" does not appear in nums. "000", "010", "100", and "110" would also be correct.
+
+ 
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 16
+    nums[i].length == n
+    nums[i] is either '0' or '1'.
+    All the strings of nums are unique.
+
+
+*/
+
+class Solution {
+    public:
+        string findDifferentBinaryString(vector<string>& nums) {
+            map<string, int> mpp;
+            for(int i = 0; i<nums.size(); i++){
+                mpp[nums[i]]++;
+            }
+            int n = nums[0].length();
+            string ans = "";
+            queue<string> q;
+            q.push("0");
+            q.push("1");
+            while(!q.empty()){
+                string s = q.front();
+                if(s.length()==n){
+                    if(!mpp[s]){
+                        return s;
+                    }
+                }
+                if(s.length()>n){
+                    return "";
+                }
+                q.pop();
+                q.push(s+"0");
+                q.push(s+"1");
+            }
+            return "";
+        }
+    };

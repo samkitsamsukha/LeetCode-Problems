b29cfbf1b729279632c807a388212550854e699f
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-03-21 08:36:22 +0530

    smth

--- 501-1000/0938_rangeSumOfBST.cpp
+++ 501-1000/0938_rangeSumOfBST.cpp
@@ -27,3 +27,31 @@ Constraints:
 
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ class Solution {
+    public:
+        int ans;
+        void f(TreeNode* root, int l, int h){
+            if(!root) return;
+            if(root->val >= l && root->val <= h){
+                ans = ans + root->val;
+            }
+            f(root->left, l, h);
+            f(root->right, l, h);
+        }
+        int rangeSumBST(TreeNode* root, int low, int high) {
+            ans = 0;
+            f(root, low, high);
+            return ans;
+        }
+    };

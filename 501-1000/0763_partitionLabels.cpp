2feda2cc8138f1b6f945250b4a177572f74162cf
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-03-31 00:31:55 +0530

    Solved 763

---
+++ 501-1000/0763_partitionLabels.cpp
@@ -0,0 +1,62 @@
+/*
+You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part. For example, the string "ababcc" can be partitioned into ["abab", "cc"], but partitions such as ["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+
+Return a list of integers representing the size of these parts.
+
+ 
+
+Example 1:
+
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+Example 2:
+
+Input: s = "eccbbbbdec"
+Output: [10]
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 500
+    s consists of lowercase English letters.
+
+
+*/
+
+class Solution {
+    public:
+        vector<int> partitionLabels(string s) {
+            int l = 0;
+            vector<int> ans;
+            map<char, int> mpp;
+            for(int i = 0; i<s.length(); i++){
+                mpp[s[i]] = i;
+            }
+            for(int i = 0; i<s.length(); i++){
+                // cout << s[i] << endl;
+                int l = i;
+                // cout << "l = " << l << endl;
+                int r = mpp[s[i]];
+                // cout << "r = " << r << endl;
+                while(l<r){
+                    if(mpp[s[l]]<=r){
+                        l++;
+                    }
+                    else{
+                        r = mpp[s[l]];
+                    }
+                }
+                ans.push_back(r-i+1);
+                i = r;
+            }
+            return ans;
+        }
+    };

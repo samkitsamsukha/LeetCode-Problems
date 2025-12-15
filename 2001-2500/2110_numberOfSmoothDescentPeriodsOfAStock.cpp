7f9d6f1d032630c209d041c20b75a83b6cb57ea2
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-12-15 08:38:18 +0530

    Solved 2110

---
+++ 2001-2500/2110_numberOfSmoothDescentPeriodsOfAStock.cpp
@@ -0,0 +1,61 @@
+/*
+You are given an integer array prices representing the daily price history of a stock, where prices[i] is the stock price on the ith day.
+
+A smooth descent period of a stock consists of one or more contiguous days such that the price on each day is lower than the price on the preceding day by exactly 1. The first day of the period is exempted from this rule.
+
+Return the number of smooth descent periods.
+
+ 
+
+Example 1:
+
+Input: prices = [3,2,1,4]
+Output: 7
+Explanation: There are 7 smooth descent periods:
+[3], [2], [1], [4], [3,2], [2,1], and [3,2,1]
+Note that a period with one day is a smooth descent period by the definition.
+
+Example 2:
+
+Input: prices = [8,6,7,7]
+Output: 4
+Explanation: There are 4 smooth descent periods: [8], [6], [7], and [7]
+Note that [8,6] is not a smooth descent period as 8 - 6 ≠ 1.
+
+Example 3:
+
+Input: prices = [1]
+Output: 1
+Explanation: There is 1 smooth descent period: [1]
+
+ 
+
+Constraints:
+
+    1 <= prices.length <= 105
+    1 <= prices[i] <= 105
+
+*/
+
+class Solution {
+public:
+    int f(int i, vector<int>& prices, vector<int>& dp){
+        if(i==0){
+            return dp[0] = 1;
+        }
+        if(dp[i]!=-1) return dp[i];
+        if(prices[i-1] - prices[i] == 1){
+            return dp[i] = 1+dp[i-1];
+        }
+        return dp[i] = 1;
+    }
+    long long getDescentPeriods(vector<int>& prices) {
+        vector<int> dp(prices.size(), -1);
+        long long sum = 0LL;
+        for(int i = 0; i<prices.size(); i++){
+            int x = f(i, prices, dp);
+            sum+=dp[i];
+        }
+        return sum;
+    }
+};

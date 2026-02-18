19435623f0ca57b4332e8f9a558c9b7fad83e964
Samkit Samsukha <samkitsamsukha@gmail.com>
2026-02-18 12:04:34 +0530

    Solved 693

---
+++ 501-1000/0693_binaryNumberWithAlternatingBits.cpp
@@ -0,0 +1,47 @@
+/*
+Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: true
+Explanation: The binary representation of 5 is: 101
+
+Example 2:
+
+Input: n = 7
+Output: false
+Explanation: The binary representation of 7 is: 111.
+
+Example 3:
+
+Input: n = 11
+Output: false
+Explanation: The binary representation of 11 is: 1011.
+
+ 
+
+Constraints:
+
+    1 <= n <= 231 - 1
+
+*/
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int b = 1;
+        if(n%2==0) b = 0;
+        n = n/2;
+        while(n>0){
+            if(n%2==b){
+                return false;
+            }
+            b = n%2;
+            n = n/2;
+        }
+        return true;
+    }
+};
